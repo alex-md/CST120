049cd4b2af4315ba5170fd3972bc7ce467a0834f
Alex Johnson <alexrnau@gmail.com>
2022-07-10 19:27:52 -0700

    ms7

--- Milestone/Website/js/scripts.js
+++ Milestone/Website/js/scripts.js
@@ -1,58 +1,30 @@
 RunFunctions();
 
 function RunFunctions() {
-    HomeImageColorShift();
-    HobbiesImageColorShift();
     IncreaseViews();
+    $("#view-count").text(AddCommas($("#view-count").text()));
 }
 
-/**
- * When you hover over the image, it will change to a random color, and when you stop hovering over the
- * image, it will change back to its original color.
- */
-function HomeImageColorShift() {
-    $(document).ready(() => {
-        $(".home-img>img").hover(function () {
-                $(this).css("filter",
-                    `hue-rotate(${Math.floor(Math.random() * 360)}deg)`);
-            } /* Making the image change back to their original color when you stop hovering over them. */,
-            function () {
-                $(this).css("filter",
-                    "hue-rotate(0deg)");
-            });
-    });
-}
-
-/**
- * When you hover over an image, it changes to a random color, and when you stop hovering over it, it
- * changes back to its original color.
- */
-function HobbiesImageColorShift() {
-    $(document).ready(() => {
-        $("body > div > div > img:nth-child(n)").hover(function () {
-                $(this).css("filter",
-                    `hue-rotate(${Math.floor(Math.random() * 360)}deg)`);
-            } /* Making the image change back to their original color when you stop hovering over them. */,
-            function () {
-                $(this).css("filter",
-                    "hue-rotate(0deg)");
-            });
-    });
-}
-
-/**
- * If the view-count is null/undefined, set it to 0. If it's not, get the view-count, increase it by 1,
- * set it to the new value, and display it on the page.
- */
+// function that adds a view count to the bottom of the page. Use localStorage to store the view count. The view count
+// will increment randomly between 1 and 188 every time a page is loaded.
 function IncreaseViews() {
+    // first generate a random number between 1 and 8
+    let randomNumber = Math.floor(Math.random() + 8) + 1;
+    // if the view count is null/undefined, set it to 0.
     if (localStorage.getItem("view-count") === null) {
-        // If the view-count is null/undefined, set it to 0.
-        localStorage.setItem("view-count",
-            0);
+        localStorage.setItem("view-count", 0);
     }
-    let viewCount = localStorage.getItem("view-count"); // Get the view-count.
-    viewCount++; // Increase the view-count by 1 on each page load.
-    localStorage.setItem("view-count",
-        viewCount); // Set the view-count to the new value.
-    $("#view-count").text(viewCount); // Set the view-count to the new value and display it on the page.
+    // get the view count
+    let viewCount = localStorage.getItem("view-count");
+    // add the random number to the view count
+    viewCount = parseInt(viewCount) + randomNumber;
+    // set the view count to the new value
+    localStorage.setItem("view-count", viewCount);
+    // display the view count on the page
+    $("#view-count").text(viewCount);
+}
+
+// add a comma to the view count every 3 digits
+function AddCommas(number) {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
